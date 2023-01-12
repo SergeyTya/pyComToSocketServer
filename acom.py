4f12d2fc0fde42f7352f0566623f73a8f4ec61f1
npt_noote <sergey_ty@mail.ru>
2023-01-12 08:47:52 +0300

    use lock

--- acom.py
+++ acom.py
@@ -7,47 +7,45 @@ lock = asyncio.Lock()
 class OutputProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         self.transport = transport
-        # print('port opened', transport)
         transport.serial.rts = False  # You can manipulate Serial object via transport
-        # transport.write(bytes([0x01, 0x03, 0x00, 0x00, 0x00, 0x05, 0x85, 0xC9]))
-      
-          # Write serial data via transport
 
     def data_received(self, data):
-        # print('data received', repr(data))
-        # self.transport.close()
-        lock.release()
-       
+        try:
+            lock.release()
+        except RuntimeError:
+            pass
 
-    # def connection_lost(self, exc):
-    #     print('port closed')
-    #     # self.transport.loop.stop()
+    def pause_writing(self):
+        print('pause writing')
+        print(self.transport.get_write_buffer_size())
 
-    # def pause_writing(self):
-    #     print('pause writing')
-    #     print(self.transport.get_write_buffer_size())
-
-    # def resume_writing(self):
-    #     print(self.transport.get_write_buffer_size())
-    #     print('resume writing')
+    def resume_writing(self):
+        print(self.transport.get_write_buffer_size())
+        print('resume writing')
 
 async def com_writer(data):
-    transport, protocol = await serial_asyncio.create_serial_connection(loop, OutputProtocol, 'COM4', baudrate=230400)
-    # transport.write(data)
     while True:
-        await lock.acquire()
-        transport.write(bytes([0x01, 0x03, 0x00, 0x00, 0x00, 0x05, 0x85, 0xC9]))
-
-
-async def cycle():
-    cnt =0 
-    while True:
-        await lock.acquire()
-        loop.create_task(com_writer(bytes([0x01, 0x03, 0x00, 0x00, 0x00, 0x05, 0x85, 0xC9])))
+        transport, protocol = await serial_asyncio.create_serial_connection(loop, OutputProtocol, 'COM4', baudrate=230400)
+        n = 0
+        while n<10:
+            if lock.locked():
+                await asyncio.sleep(0.03)
+                n = n + 1
+            else:
+                await lock.acquire()
+                n = 0
+                transport.write(bytes([0x01, 0x03, 0x00, 0x00, 0x00, 0x05, 0x85, 0xC9]))
+        n=0
+        transport.close()
+        print('Timeout!')
+        await asyncio.sleep(1)
+        try:
+            lock.release()
+        except RuntimeError:
+            pass
 
 
 loop = asyncio.get_event_loop()
-# loop.run_until_complete(cycle())
 loop.run_until_complete(com_writer(0))
 loop.run_forever()
 loop.close()
